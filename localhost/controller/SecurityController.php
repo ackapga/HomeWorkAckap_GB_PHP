68f753b5c91b62bab96f212a532697b2fff34245
Ackap <ackapga@gmail.com>
2022-06-01 00:13:22 +0500

    Практическая работа - База данных, сайт localhost

---
+++ localhost/controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'model/UserProvider.php';
+$pdo = require 'db.php'; // Подключим PDO
+
+session_start();
+
+$error = null;
+
+if (isset($_GET['action']) && $_GET['action'] === 'logout') {
+    unset($_SESSION['username']);
+    unset($_SESSION['user_id']);
+    header("Location: index.php");
+    die();
+}
+
+if (isset($_POST['username'], $_POST['password'])) {
+    ['username' => $username, 'password' => $password] = $_POST;
+    $userProvider = new UserProvider($pdo);
+    $user = $userProvider->getByUsernameAndPassword($username, $password);
+
+    if ($user === null) {
+        $error = 'Пользователь с указанными учетными данными не найден';
+    } else {
+        $_SESSION['username'] = $user;
+        $_SESSION['user_id'] = $user->getId();
+        header("Location: index.php");
+        die();
+    }
+}
+
+include "view/signin.php";
